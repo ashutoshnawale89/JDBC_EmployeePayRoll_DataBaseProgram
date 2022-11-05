5f8d77e7ea76df34cb63c1c27a80970ae410f026
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-11-05 13:04:24 +0530

    Added:  Create Connection To The DataBase

--- src/main/java/com/program/JdbcProgram.java
+++ src/main/java/com/program/JdbcProgram.java
@@ -6,9 +6,46 @@ import java.sql.SQLException;
 import java.util.Enumeration;
 
 public class JdbcProgram {
-	
-	public static void main(String[] args) throws SQLException {
-		System.out.println("Welcome to JDBC Program");
+	public static void DriverLoaded() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Driver loaded");
+		} catch(ClassNotFoundException e  ) {
+			throw new IllegalStateException ("can find the driver in class path!", e);
+		}
+	}
+
+	public static void ListDrivers() {
+		Enumeration<java.sql.Driver> driverList= DriverManager.getDrivers();
+		while (driverList.hasMoreElements()) {
+			java.sql.Driver driverClass =(java.sql.Driver) driverList.nextElement();
+			System.out.println("  "+driverClass.getClass());
+		}
+	}
+
+	public static void ConnectionCreate() throws MySqlConnectionExceptionHandle {
+		String jdbcUrl = "jdbc:mysql://localhost:3306/employee_payroll";
+		String userName = "root";
+		String password = "Ashu$1998";
+		Connection connection;
+		try 
+		{
+			connection=DriverManager.getConnection(jdbcUrl,userName,password);
+			if (connection.isClosed()) {
+				System.out.println("connection is closed");
+			}  else {
+				System.out.println("connection created ......");
+			}
+			connection.close();
+		}  catch(Exception e  ) {
+			throw new MySqlConnectionExceptionHandle(MySqlConnectionExceptionHandle.ExceptionType.ENTERED_DATA_NOT_CONNECTED_WRONG_INPUTS,"Enter Correct Input");
+			 }
+	}
+
+	public static void main(String[] args) throws MySqlConnectionExceptionHandle {
+		DriverLoaded();
+		ListDrivers();
+		ConnectionCreate();
 	}
 }
 
