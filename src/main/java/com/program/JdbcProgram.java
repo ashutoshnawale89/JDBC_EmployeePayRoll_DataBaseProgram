f06471a907182472d47ec334526417d9a391d260
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-11-05 14:47:38 +0530

    Refactor: Update Salary and Test case Write using Prepared Statement

--- src/main/java/com/program/JdbcProgram.java
+++ src/main/java/com/program/JdbcProgram.java
@@ -2,16 +2,17 @@ package com.program;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Enumeration;
+
 
 public class JdbcProgram {
 
 
-	public static boolean ConnectionCreate(String query) throws MySqlConnectionExceptionHandle {
-		boolean result=true;
+
+	public static boolean ConnectionCreate(String names,int bas_pay) throws MySqlConnectionExceptionHandle {
+		boolean result;
 		String jdbcUrl = "jdbc:mysql://localhost:3306/payroll";
 		String userName = "root";
 		String password = "Ashu$1998";
@@ -24,9 +25,22 @@ public class JdbcProgram {
 			}  else {
 				System.out.println("connection created ......");
 			}
-			Statement statement = connection.createStatement();
-          result=statement.execute(query);			
 			
+			String q="update employee_payroll set salary=?  where name=?";
+			
+			int basicPay=bas_pay;
+			String name=names;
+			
+			PreparedStatement pstmt=connection.prepareStatement(q);
+			// put the value to change it
+			pstmt.setLong(1, basicPay);
+			pstmt.setNString(2,name);			
+		    pstmt.executeUpdate();
+		    result=true;
+			System.out.println("Done...");
+
+			
+			Statement statement = connection.createStatement();
 			ResultSet resultSet = statement.executeQuery("SELECT*FROM employee_payroll");
 			 while (resultSet.next()) {
                  System.out.println(resultSet.getInt("id") + "  " + resultSet.getString("name") + "   " + resultSet.getString("gender") + "  " + resultSet.getString("salary") + "  " +resultSet.getString("start_Date") );
@@ -37,6 +51,12 @@ public class JdbcProgram {
 			 }
 		return result;
 	}
+
+//public static void main(String[] args) throws MySqlConnectionExceptionHandle {
+//	JdbcProgram obj=new JdbcProgram();
+//	System.out.println(obj.ConnectionCreate("Terisa", 700000));
+//}
 }
+
 
 
--- src/test/java/JdbcQueryTestCase.java
+++ src/test/java/JdbcQueryTestCase.java
@@ -1,14 +1,14 @@
 import org.junit.Assert;
 import org.junit.Test;
+
 import com.program.JdbcProgram;
 import com.program.MySqlConnectionExceptionHandle;
 
+
 public class JdbcQueryTestCase {
 @Test
-public void jdbc_Program_QueryTestCase() throws MySqlConnectionExceptionHandle {
-	JdbcProgram obj=new JdbcProgram();
-	String query="update employee_payroll set salary=350000 where name='Terisa';";
-	
-	Assert.assertEquals(false, obj.ConnectionCreate(query));
+public void jdbc_Program_UpdateQueryTestCase() throws MySqlConnectionExceptionHandle {
+	JdbcProgram obj=new JdbcProgram();	
+	Assert.assertEquals(true, obj.ConnectionCreate("Terisa", 3000000));
 }
 }
