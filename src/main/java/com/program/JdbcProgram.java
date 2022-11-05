fa63d65681be38e931afdec3d7776212c6084cb9
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-11-05 15:09:19 +0530

    Refactor:  Retrieve DB using Prepared Statement

--- src/main/java/com/program/JdbcProgram.java
+++ src/main/java/com/program/JdbcProgram.java
@@ -11,7 +11,7 @@ public class JdbcProgram {
 
 
 
-	public static boolean ConnectionCreate(String names,int bas_pay) throws MySqlConnectionExceptionHandle {
+	public static ConnectionCreate(String names,int bas_pay) throws MySqlConnectionExceptionHandle {
 		boolean result;
 		String jdbcUrl = "jdbc:mysql://localhost:3306/payroll";
 		String userName = "root";
@@ -26,36 +26,40 @@ public class JdbcProgram {
 				System.out.println("connection created ......");
 			}
 			
-			String q="update employee_payroll set salary=?  where name=?";
+			String q="select * from employee_payroll where name=?";
 			
-			int basicPay=bas_pay;
 			String name=names;
 			
 			PreparedStatement pstmt=connection.prepareStatement(q);
 			// put the value to change it
-			pstmt.setLong(1, basicPay);
-			pstmt.setNString(2,name);			
-		    pstmt.executeUpdate();
-		    result=true;
+			pstmt.setNString(1,name);			
+		    pstmt.executeQuery();
 			System.out.println("Done...");
-
-			
-			Statement statement = connection.createStatement();
-			ResultSet resultSet = statement.executeQuery("SELECT*FROM employee_payroll");
+			ResultSet resultSet = pstmt.executeQuery();
 			 while (resultSet.next()) {
-                 System.out.println(resultSet.getInt("id") + "  " + resultSet.getString("name") + "   " + resultSet.getString("gender") + "  " + resultSet.getString("salary") + "  " +resultSet.getString("start_Date") );
+                 System.out.println(resultSet.getInt("id") + "  "
+			                       + resultSet.getString("name") + "   "
+                		           + resultSet.getString("gender") + "  " 
+			                       + resultSet.getString("salary") + "  " 
+                		           +resultSet.getString("start_Date") );
              }
+			
+//			Statement statement = connection.createStatement();
+//			ResultSet resultSet = statement.executeQuery("SELECT*FROM employee_payroll");
+//			 while (resultSet.next()) {
+//                 System.out.println(resultSet.getInt("id") + "  " + resultSet.getString("name") + "   " + resultSet.getString("gender") + "  " + resultSet.getString("salary") + "  " +resultSet.getString("start_Date") );
+//             }
 			connection.close();
 		}  catch(Exception e  ) {
 			throw new MySqlConnectionExceptionHandle(MySqlConnectionExceptionHandle.ExceptionType.ENTERED_DATA_NOT_CONNECTED_WRONG_INPUTS,"Enter Correct Input");
 			 }
-		return result;
+		
 	}
 
-//public static void main(String[] args) throws MySqlConnectionExceptionHandle {
-//	JdbcProgram obj=new JdbcProgram();
-//	System.out.println(obj.ConnectionCreate("Terisa", 700000));
-//}
+public static void main(String[] args) throws MySqlConnectionExceptionHandle {
+	JdbcProgram obj=new JdbcProgram();
+	System.out.println(obj.ConnectionCreate("Terisa", 700000));
+}
 }
 
 
