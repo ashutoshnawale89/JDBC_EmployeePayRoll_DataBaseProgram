5b634f1aeeeb9b5cb70e658737ff00a40f8af96b
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-11-05 13:38:06 +0530

    Refactor: Retrieve All DB Table

--- src/main/java/com/program/JdbcProgram.java
+++ src/main/java/com/program/JdbcProgram.java
@@ -2,13 +2,42 @@ package com.program;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Enumeration;
 
 public class JdbcProgram {
-	
-	public static void main(String[] args){
-		System.out.println("WELCOME TO JDBC PROGRAM");
+
+
+	public static void ConnectionCreate() throws MySqlConnectionExceptionHandle {
+		String jdbcUrl = "jdbc:mysql://localhost:3306/payroll";
+		String userName = "root";
+		String password = "Ashu$1998";
+		Connection connection;
+		try 
+		{
+			connection=DriverManager.getConnection(jdbcUrl,userName,password);
+			if (connection.isClosed()) {
+				System.out.println("connection is closed");
+			}  else {
+				System.out.println("connection created ......");
+			}
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery("SELECT*FROM employee_payroll");
+			 while (resultSet.next()) {
+                 System.out.println(resultSet.getInt("id") + "  " + resultSet.getString("name") + "   " + resultSet.getString("gender") + "  " + resultSet.getString("salary") + "  " +resultSet.getString("start_Date") );
+             }
+			connection.close();
+		}  catch(Exception e  ) {
+			throw new MySqlConnectionExceptionHandle(MySqlConnectionExceptionHandle.ExceptionType.ENTERED_DATA_NOT_CONNECTED_WRONG_INPUTS,"Enter Correct Input");
+			 }
+	}
+
+	public static void main(String[] args) throws MySqlConnectionExceptionHandle {
+		System.out.println("Welcome To The JDBC Program");
+		ConnectionCreate();
+
 	}
 }
 
