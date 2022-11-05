e9d7a0698b476f92ebee0e6dd1b61b5f3434260a
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-11-05 12:58:05 +0530

    Added: Print The Welcome Msg

---
+++ src/main/java/com/program/JdbcProgram.java
@@ -0,0 +1,15 @@
+package com.program;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
+public class JdbcProgram {
+	
+	public static void main(String[] args) throws SQLException {
+		System.out.println("Welcome to JDBC Program");
+	}
+}
+
+
